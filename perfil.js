704b4cb5fba732977ab03b7d8bb87b4eb0709f96
Pedro Lino <oagregarural@gmail.com>
2026-04-08 16:31:38 -0300

    Login Cadasatro + javascript

--- perfil.js
+++ perfil.js
@@ -1,6 +1,18 @@
-const btncadastro = document.getElementById("btnCadastro");
-const authentication = document.getElementById("elementoAuthetication");
 
-btncadastro.addEventListener( "click", () => {
-    authentication.remove()
+const TelaLogin = document.getElementById("idTelaLogin");
+const Telacadastro = document.getElementById("idTelaCadastro");
+
+const btnToCadastro = document.getElementById("btnToCadastro");
+const btnToLogin = document.getElementById("btnToLogin");
+
+Telacadastro.classList.add("oculto")
+
+btnToLogin.addEventListener( "click", () => {
+    Telacadastro.classList.add("oculto") 
+    TelaLogin.classList.remove("oculto")
+});
+
+btnToCadastro.addEventListener( "click", () => {
+    TelaLogin.classList.add("oculto")
+    Telacadastro.classList.remove("oculto") 
 });
