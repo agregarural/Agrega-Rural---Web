1c06e971364e5d2bdc0c10e9887a9061c81892eb
LnPedro <pedroabraao1611@gmail.com>
2026-06-02 00:59:13 -0300

    pedro

--- js/cooperativa.js
+++ js/cooperativa.js
@@ -1,11 +1,31 @@
- fetch('/components/header.html')
-        .then(res => res.text())
-        .then(html => {
-            document.getElementById('header-placeholder').innerHTML = html;
-        });
 
- fetch('/components/menuoptions.html')
-        .then(res => res.text())
-        .then(html => {
-            document.getElementById('menuOptions').innerHTML = html;
-        });
+
+
+fetch('/components/header.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('header-placeholder').innerHTML = html;
+    });
+
+fetch('/components/menuoptions.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('menuOptions').innerHTML = html;
+    });
+
+
+const btnAddAdm = document.getElementById("btnAddAdm");
+const formularioOverlay = document.getElementById("overlay");
+
+btnAddAdm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (formularioOverlay.classList.contains("oculto")) {
+
+        formularioOverlay.classList.remove("oculto");
+    }
+    else {
+        formularioOverlay.classList.add("oculto");
+    }
+
+});
